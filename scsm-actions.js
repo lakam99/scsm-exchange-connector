63f200a2d07bcb49c087ce4da286e93914e79334
Arkam Mazrui <superarkam@gmail.com>
2025-04-14 13:58:50 -0400

    add scsm-actions

---
+++ scsm-actions.js
@@ -0,0 +1,21 @@
+function getUser(name, email) {
+    return {};
+}
+
+function createUser(name, email) {
+    return true;
+}
+
+function createTicket(Title, Description, assignedUser, email) {
+    return {
+        Title,
+        Description
+    }
+}
+
+// function getTemplate(id) {
+//     return {
+//         Title: ''
+//     }
+// }
+
